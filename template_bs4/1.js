71725a5cd721985646e9785c1a9c09c399beb7f6
thuham2k4 <dinhthithu20042k1@gmail.com>
2022-01-27 17:36:40 +0700

    thu ham

---
+++ template_bs4/1.js
@@ -0,0 +1,15 @@
+﻿ $(function() {
+     khoimonan = $('.nhieumon').isotope({
+         // options
+         itemSelector: '.motmon',
+         layoutMode: 'masonry'
+     });
+     $('.tieudect a').click(function() {
+         dulieu = $(this).data('monan');
+         console.log(dulieu);
+         khoimonan.isotope({ filter: dulieu })
+         return false;
+         // click vào a thì nó ko làm gi het
+     });
+     new WOW().init();
+ })
